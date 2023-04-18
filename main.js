734179eb866a124299b10eb831dba1e865953004
MexCollen <131192277+MexCollen@users.noreply.github.com>
2023-04-18 12:27:52 -0500

    Update main.js

--- main.js
+++ main.js
@@ -1,1 +1,8 @@
 const h1Principal = document.querySelector("#miH1");
+const miButton = document.querySelector("#miButton");
+
+miButton.addEventListener("click", changeText);
+
+function changeText () {
+  h1Principal.innerText = "lorem";
+}
